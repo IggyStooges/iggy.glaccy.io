e81282445aa53b738c5210e14a5d24a63c0cd782
Dmitriy Lobasov <36813348+IggyStooges@users.noreply.github.com>
2018-03-27 13:02:44 +0300

    сделал анимацию при ошибки полей

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 var link = document.querySelector(".search-hotel-btn");
-var popup = document.querySelector(".booking-window");
-  
+  var popup = document.querySelector(".booking-window");
+  var form = document.querySelector("form");
+var depdate = document.querySelector("[name=date-departure]");
+  var arrdate = document.querySelector("[name=date-arrival]");
+  var adults = document.querySelector("[name=adults]");
+  var children = document.querySelector("[name=children]");
+
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("booking-window-show");
-});
+    evt.preventDefault();
+    popup.classList.toggle("booking-window-show");
+  });
+
+
+  form.addEventListener("submit", function (evt) {
+    if (!arrdate.value || !adults.value || !depdate.value || !children.value) {
+      evt.preventDefault();
+      popup.classList.add("input-error");
+    } 
+  });
