5b0f8b338427cbef88a7960bd557ea46d1518595
Dmitriy Lobasov <36813348+IggyStooges@users.noreply.github.com>
2018-08-06 20:45:42 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+var sliderButtonPrev = document.querySelector(".prev-slider-btn");
+var sliderButtonNext = document.querySelector(".next-slider-btn");
+var activeSlider = document.querySelector(".active-slider");
+var disactiveSlider = document.querySelector(".disactive-slider");
+
+
+document.documentElement
+    .classList.replace('no-js', 'js');
+
+sliderButtonPrev.addEventListener("click", function (evt) {
+    evt.preventDefault();   
+        disactiveSlider.classList.add("active-slider");
+    disactiveSlider.classList.remove("disactive-slider");
+    activeSlider.classList.add("disactive-slider");
+    activeSlider.classList.remove("active-slider");
+});
+
+
+sliderButtonNext.addEventListener("click", function (evt) {
+    evt.preventDefault();   
+        disactiveSlider.classList.add("active-slider");
+    disactiveSlider.classList.remove("disactive-slider");
+    activeSlider.classList.add("disactive-slider");
+    activeSlider.classList.remove("active-slider");
+});
 
